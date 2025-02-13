6db27fd91938b29155947769568ee809cdd60c71
Fabio Flauzino <fabio@mfrural.com.br>
2025-02-13 16:02:25 -0300

    Populando Select

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import { Input } from "../components/Input";
 import { Select } from "../components/Select";
+import { CATEGORIES, CATEGORIES_KEYS } from "../utils/categories";
 
 export default function Refund() {
+  const [category, setCategory] = useState("")
   return (
     <form className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w-[512px]">
       <header>
@@ -9,16 +12,24 @@ export default function Refund() {
         <p className="text-sm text-gray-200 mt-2 mb-4">Dados da despesa para solicitar reembolso.</p>
       </header>
 
-      <Input 
+      <Input
         required
         legend="Nome da solicitação"
       />
 
-      <Select 
+      <Select
         required
-        legend="Categoria"  
+        legend="Categoria"
+        value={category}
+        onChange={(e) => setCategory(e.target.value)}
       >
-        {/* Aqui irá as opçoes */}
+        {
+          CATEGORIES_KEYS.map((category, index) => (
+            <option value={category} key={index}>
+              {CATEGORIES[category].name}
+            </option>
+          ))
+        }
       </Select>
     </form>
   )
