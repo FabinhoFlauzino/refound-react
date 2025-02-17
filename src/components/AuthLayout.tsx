dbc66ee433f48b292237b4b6b97279c15fe0c329
Fabio Flauzino <fabio@mfrural.com.br>
2025-02-17 18:28:40 -0300

    Componente Loading

--- src/components/AuthLayout.tsx
+++ src/components/AuthLayout.tsx
@@ -6,11 +6,11 @@ export function AuthLayout() {
   return (
     <div className="
       w-screen h-screen bg-gray-400 text-gray-100
-      flex flex-col justify-center items-center
+      flex flex-col justify-center items-center p-8
     ">
       <main className="
         bg-gray-500 p-8 rounded-md flex items-center flex-col
-        md:min-w-[462px]
+        w-full md:max-w-[462px]
       ">
         <img src={logoSvg} alt="Logo" className="my-8" />
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,8 +2,15 @@ import { BrowserRouter } from "react-router";
 import { AuthRoutes } from "./AuthRoutes";
 import { EmployeeRoutes } from "./EmployeeRoutes";
 import { ManagerRoutes } from "./ManagerRoutes";
+import { Loading } from "../components/Loading";
+
+const isLoading = false
 
 export function Routes() {
+  if(isLoading) {
+    return <Loading />
+  }
+
   return (
     <BrowserRouter>
       {/* <AuthRoutes /> */}
