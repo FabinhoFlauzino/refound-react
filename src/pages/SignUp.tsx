a6a32ca3058cd6d33a91e4c45c73156dd5675d2a
Fabio Flauzino <fabio@mfrural.com.br>
2025-03-05 14:55:59 -0300

    Instalando e configurando Zod na página SignUp

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,6 +1,17 @@
 import { useState } from "react";
 import Button from "../components/Button";
 import { Input } from "../components/Input";
+import { z, ZodError } from "zod";
+
+const signUpSchema = z.object({
+  name: z.string().trim().min(1, { message: "Informe o nome" }),
+  email: z.string().trim().email({ message: "E-mail inválido" }),
+  password: z.string().min(6, { message: "Senha deve ter pelo menos 6 dígitos" }),
+  passwordConfirm: z.string({ message: "Confirme a senha" })
+}).refine((data) => data.password === data.passwordConfirm, {
+  message: "As senhas não são iguais",
+  path: ["passwordConfirm"]
+})
 
 export function SignUp() {
   const [name, setName] = useState("")
@@ -13,7 +24,21 @@ export function SignUp() {
   function onSubmit(e: React.FormEvent) {
     e.preventDefault()
 
-    console.log(name, email, password, passwordConfirm)
+    const data = signUpSchema.parse({
+      name, email, password, passwordConfirm
+    })
+
+    try {
+      setIsLoading(true)
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return alert(error.issues[0].message)
+      }
+
+      alert("Não possível cadastrar")
+    } finally {
+      setIsLoading(false)
+    }
   }
 
   return (
@@ -24,7 +49,7 @@ export function SignUp() {
         required
         legend="Name"
         placeholder="Seu Nome"
-        value={email}
+        value={name}
         onChange={(e) => setName(e.target.value)}
       />
 
@@ -51,7 +76,7 @@ export function SignUp() {
         legend="Confirmação da Senha"
         type="password"
         placeholder="123456"
-        value={password}
+        value={passwordConfirm}
         onChange={(e) => setPasswordConfirm(e.target.value)}
       />
 
