e6665ccab182a01eb7d57ccd8c5ea71f39711dd2
Fabio Flauzino <fabio@mfrural.com.br>
2025-03-06 18:04:02 -0300

    Carregando dados do localStorage

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,11 +5,9 @@ import { ManagerRoutes } from "./ManagerRoutes";
 import { Loading } from "../components/Loading";
 import { useAuth } from "../hooks/useAuth";
 
-const isLoading = false
-
 export function Routes() {
 
-  const { session } = useAuth()
+  const { session, isLoading } = useAuth()
 
   function Route() {
     switch (session?.user.role) {
